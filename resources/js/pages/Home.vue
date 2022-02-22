dfb060db22a7048ba2f0524247037ea155187f61
Yu4iy <yurimohyliuk@gmail.com>
2022-02-22 12:30:25 +0100

    project-setup

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+	  HOME
+  </h1>
+</template>
+
+<script>
+export default {
+	name:'Home',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<section>
+		<main>
+			<router-view></router-view>
+		</main>
+	</section>
+</template>
+
+<script>
+	export default {
+		name:'App',
+		components:{
+		},
+	}
+</script>
+
+<style lang="scss">
+</style>
