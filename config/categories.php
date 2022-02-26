3f62b31db3b88c5fea35c678e5c27b46728989f8
sofMD <xsofiamaria.32@gmail.com>
2022-02-26 09:59:06 +0100

    category_user-data

--- config/categories.php
+++ config/categories.php
@@ -1,85 +1,106 @@
 <?php 
 return [
+    // 1
     [
         'name' => 'Pizza',
         'image' => 'https://media-assets.vanityfair.it/photos/61e444841e21bc3bd54b5357/1:1/w_2832,h_2832,c_limit/pizza%20tendenze.jpg',
     ],
+    // 2
     [
         'name' => 'Cinese',
         'image' => 'https://www.buttalapasta.it/wp-content/uploads/2011/01/cucina-cinese-piatti-tipici-e-specialita.jpg',
     ],
+    // 3
     [
         'name' => 'Giapponese',
         'image' => 'https://www.travelstales.it/wp-content/uploads/2018/10/ramen.jpg',
     ],
+    // 4
     [
         'name' => 'Indiano',
         'image' => 'https://media-cdn.tripadvisor.com/media/photo-s/1b/d3/9e/d8/getlstd-property-photo.jpg',
     ],
+    // 5
     [
         'name' => 'Italiano',
         'image' => 'https://www.progettoartes.it/wp-content/uploads/2018/01/2018-anno-del-cibo-italiano.jpg',
     ],
+    // 6
     [
         'name' => 'Greco',
         'image' => 'https://www.sfizioso.it/wp-content/uploads/2021/06/piatti-tipici-greci.jpg',
     ],
+    // 7
     [
         'name' => 'Americano',
         'image' => 'https://www.finefoodgroup.com/wp-content/uploads/2020/06/cucina-americana-899x620.jpg',
     ],
+    // 8
     [
         'name' => 'Spagnolo',
         'image' => 'https://www.bioimis.it/resize_image?id=e4cd7759-dfd7-47ed-9aa9-19234f952c59&w=791&h=500',
     ],
+    // 9
     [
         'name' => 'Piadina',
         'image' => 'https://static.italiaoggi.it/content_upload/img/2263/95/2263952/piadina-717474.jpg',
     ],
+    // 10
     [
         'name' => 'Fast-Food',
         'image' => 'https://www.melarossa.it/wp-content/uploads/2020/03/junk-food-altera-memoria-controllo-appetito.jpg?x75642',
     ],
+    // 11
     [
         'name' => 'Poke',
         'image' => 'https://primochef.it/wp-content/uploads/2019/12/SH_poke_2.jpg',
     ],
+    // 12
     [
         'name' => 'Kebab',
         'image' => 'https://www.innaturale.com/wp-content/uploads/2017/11/panino-kebab.jpg',
     ],
+    // 13
     [
         'name' => 'Pasticceria',
         'image' => 'https://www.focus.it/images/2021/11/12/pasticcini_1020x680.jpg',
     ],
+    // 14
     [
         'name' => 'Messicano',
         'image' => 'https://www.viaggiaregratis.eu/wp-content/uploads/2020/11/Cucina-Messicana.jpg',
     ],
+    // 15
     [
         'name' => 'Barbecue',
         'image' => 'https://www.lamacchinadelgusto.it/wp-content/uploads/2018/05/barbecue_griglia.jpg',
     ],
+    // 16
     [
         'name' => 'Hamburgher',
         'image' => 'https://www.sfizioso.it/wp-content/uploads/2018/06/ricette-hamburger.jpg',
     ],
+    // 17
     [
         'name' => 'Burrito',
         'image' => 'https://www.melarossa.it/wp-content/uploads/2021/07/burrito-messicano-1280x720.jpg',
     ],
+    // 18
     [
         'name' => 'Dessert',
         'image' => 'https://www.ilgiornaledelcibo.it/wp-content/uploads/2018/04/Categoria-dessert.jpg',
     ],
+    // 19
     [
         'name' => 'Gelato',
         'image' => 'https://www.calendariodelciboitaliano.it/wp-content/uploads/2017/06/gelatoheader.jpg',
     ],
+    // 20
     [
         'name' => 'Pasta',
         'image' => 'https://www.melarossa.it/wp-content/uploads/2016/09/Pasta-il-cibo-perfetto-a-dieta.jpg',
     ],
+    // 21
     [
         'name' => 'Sushi',
         'image' => 'https://www.stateofmind.it/wp-content/uploads/2020/05/Nyotaimori-e-body-sushi-la-pratica-e-la-sua-diffusione-in-occidente.jpg',
