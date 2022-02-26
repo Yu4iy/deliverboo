281f78a24e955eb13e65ee7d279129d116b7df4e
Nuccia955 <elenamunaro1995@gmail.com>
2022-02-26 16:33:27 +0100

    add category_user seeder

---
+++ config/pivot_category_user.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    [
+        'user_id' => 1,
+        'category_id' => 11
+    ],
+    [
+        'user_id' => 2,
+        'category_id' => 7
+    ],
+    [
+        'user_id' => 2,
+        'category_id' => 10
+    ],
+    [
+        'user_id' => 2,
+        'category_id' => 16
+    ],
+    [
+        'user_id' => 3,
+        'category_id' => 9
+    ],
+    [
+        'user_id' => 3,
+        'category_id' => 14
+    ],
+    [
+        'user_id' => 3,
+        'category_id' => 20
+    ],
+    [
+        'user_id' => 4,
+        'category_id' => 2
+    ],
+    [
+        'user_id' => 4,
+        'category_id' => 3
+    ],
+    [
+        'user_id' => 4,
+        'category_id' => 21
+    ],
+    [
+        'user_id' =>5,
+        'category_id' => 1
+    ],
+    [
+        'user_id' => 5,
+        'category_id' => 5
+    ],
+    [
+        'user_id' => 6,
+        'category_id' => 5
+    ],
+    [
+        'user_id' => 6,
+        'category_id' => 18
+    ],
+    [
+        'user_id' => 6,
+        'category_id' => 20
+    ],
+];
