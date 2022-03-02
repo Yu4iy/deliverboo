ff9de5155f785422e5926b5bf5e49904e13f9bfd
elepot87 <eleonorapotente@gmail.com>
2022-03-02 23:14:07 +0100

    aggiunta colonna per team

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -3,7 +3,7 @@
         <div class="footer-top">
             <div class="container px-4">
                 <div class="row gx-5">
-                    <div class="col-xs col-md-6 col-lg-3 col-footer">
+                    <div class="col-xs col-md-6 col-lg-4 col-xl col-footer">
                         <div class="p-3 content-col">
                             <h5 class="title-col">Scopri DeliveBoo</h5>
                             <ul class="menu-footer">
@@ -31,7 +31,7 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-xs col-md-6 col-lg-3 col-footer">
+                    <div class="col-xs col-md-6 col-lg-4 col-xl col-footer">
                         <div class="p-3 content-col">
                             <h5 class="title-col">Note legali</h5>
                             <ul class="menu-footer">
@@ -47,7 +47,7 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-xs col-md-6 col-lg-3 col-footer">
+                    <div class="col-xs col-md-6 col-lg-4 col-xl col-footer">
                         <div class="p-3 content-col">
                             <h5 class="title-col">Aiuto</h5>
                             <ul class="menu-footer">
@@ -68,7 +68,7 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-xs col-md-6 col-lg-3 col-footer">
+                    <div class="col-xs col-md-6 col-lg-4 col-xl col-footer">
                         <div class="p-3 content-col">
                             <h5 class="title-col">Porta DeliveBoo con te</h5>
                             <ul class="menu-footer">
@@ -93,10 +93,92 @@
                             </ul>
                         </div>
                     </div>
+                    <div class="col-xs col-md-6 col-lg-4 col-xl col-footer">
+                        <div class="p-3 content-col">
+                            <h5 class="title-col">Made by</h5>
+                            <ul class="menu-footer">
+                                <li class="menu-footer-item">
+                                    <a
+                                        href="https://www.linkedin.com/in/sofia-maria-durante-a75143231/"
+                                        target="_blank"
+                                        class="name-team"
+                                    >
+                                        Sofia Durante
+                                        <i
+                                            class="fa-solid fa-terminal icon-team"
+                                        ></i>
+                                    </a>
+                                </li>
+                                <li class="menu-footer-item">
+                                    <a
+                                        href="https://www.linkedin.com/in/yuri-mohyliyuk-504923230/"
+                                        target="_blank"
+                                        class="name-team"
+                                    >
+                                        Yuri Mohyliyuk
+                                        <i
+                                            class="fa-solid fa-laptop-code icon-team"
+                                        ></i>
+                                    </a>
+                                </li>
+                                <li class="menu-footer-item">
+                                    <a
+                                        href="https://www.linkedin.com/in/elena-munaro-b8658b232/"
+                                        target="_blank"
+                                        class="name-team"
+                                    >
+                                        Elena Munaro
+                                        <i
+                                            class="fa-solid fa-code icon-team"
+                                        ></i>
+                                    </a>
+                                </li>
+                                <li class="menu-footer-item">
+                                    <a
+                                        href="https://www.linkedin.com/in/eleonora-potente/"
+                                        target="_blank"
+                                        class="name-team"
+                                    >
+                                        Eleonora Potente
+                                        <i
+                                            class="fa-solid fa-file-code icon-team"
+                                        ></i>
+                                    </a>
+                                </li>
+                                <li class="menu-footer-item">
+                                    <a
+                                        href="https://www.linkedin.com/in/jacopo-ferroni/"
+                                        target="_blank"
+                                        class="name-team"
+                                    >
+                                        Jacopo Ferroni
+                                        <i
+                                            class="fa-solid fa-ghost icon-team"
+                                        ></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="footer-copyrights">Copyrights</div>
+        <div class="footer-social">
+            <div class="container">
+                <ul class="icons-social">
+                    <li class="icon-social-item">
+                        <i class="fa-brands fa-facebook icon-footer"></i>
+                    </li>
+                    <li class="icon-social-item">
+                        <i class="fa-brands fa-twitter icon-footer"></i>
+                    </li>
+                    <li class="icon-social-item">
+                        <i class="fa-brands fa-instagram icon-footer"></i>
+                    </li>
+                </ul>
+                <div class="copyrights">Made with &hearts; by Team 4</div>
+            </div>
+        </div>
     </footer>
 </template>
 
@@ -111,35 +193,113 @@ footer {
     padding: 30px 0;
     background-color: #2e3333;
     color: #fff;
-    .col-footer {
-        margin-top: 20px;
-        .content-col {
-            height: 285px;
-            background-color: #434848;
-            .title-col {
-                font-weight: 700;
-            }
-            .menu-footer {
-                list-style: none;
-                margin: 0;
-                .menu-footer-item {
-                    margin-top: 10px;
-                    a {
-                        color: #fff;
-                        .badge-footer {
-                            max-width: 50%;
+    .footer-top {
+        .container {
+            .row {
+                justify-content: center;
+                .col-footer {
+                    margin-top: 20px;
+                    .content-col {
+                        height: 285px;
+                        background-color: #434848;
+                        .title-col {
+                            font-weight: 700;
+                        }
+                        .menu-footer {
+                            list-style: none;
+                            margin: 0;
+                            .menu-footer-item {
+                                margin-top: 10px;
+                                a {
+                                    color: #fff;
+                                    .badge-footer {
+                                        max-width: 65%;
+                                    }
+                                    .icon-team {
+                                        margin-left: 10px;
+                                    }
+                                }
+                                a:hover {
+                                    color: #00ccbc;
+                                    text-decoration: none;
+                                }
+                            }
                         }
                     }
-                    a:hover {
-                        color: #00ccbc;
-                        text-decoration: none;
+                }
+                .col-footer:last-child {
+                    margin-bottom: 20px;
+                }
+            }
+        }
+    }
+    .footer-social {
+        .container {
+            display: flex;
+            justify-content: space-between;
+        }
+        .icons-social {
+            list-style: none;
+            margin: 0;
+            display: flex;
+            padding: 0 20px;
+            .icon-social-item {
+                margin-right: 10px;
+                .icon-footer {
+                    font-size: 16px;
+                }
+            }
+            .icon-social-item:hover {
+                color: #00ccbc;
+            }
+        }
+    }
+}
+@media screen and (min-width: 992px) and (max-width: 1199px) {
+    footer {
+        .footer-top {
+            .container {
+                .row {
+                    .col-footer {
+                        .content-col {
+                            .menu-footer {
+                                .menu-footer-item {
+                                    a {
+                                        .badge-footer {
+                                            max-width: 50%;
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-    .col-footer:last-child {
-        margin-bottom: 20px;
+}
+
+@media screen and (max-width: 991px) {
+    footer {
+        .footer-top {
+            .container {
+                .row {
+                    .col-footer {
+                        .content-col {
+                            .menu-footer {
+                                .menu-footer-item {
+                                    a {
+                                        .badge-footer {
+                                            max-width: 30%;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 </style>
