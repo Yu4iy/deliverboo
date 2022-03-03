5d3c2022d962cd92f15ebd226450ff61f40e3a4e
Nuccia955 <elenamunaro1995@gmail.com>
2022-03-03 10:59:55 +0100

    edit db queries for api routes

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -11,13 +11,13 @@ class RestaurantController extends Controller
 {
 	public function index($slug)
 	{
-		$restaurants = Category::where('slug', $slug)->with('users')->get();
+		$restaurants = Category::where('slug', $slug)->with('users:id,restaurant_name,slug,address,city,iva,description,image,delivery_price')->get();
 		return response()->json($restaurants);
 	}
 
 	public function show($slug)
 	{
-		$dishes = User::where('slug', $slug)->with('dishes')->get();
+		$dishes = User::where('slug', $slug)->select('id', 'restaurant_name', 'slug', 'address', 'city' ,'iva', 'description', 'image', 'delivery_price')->with('dishes')->get();
 		return response()->json($dishes);
 	}
 }
