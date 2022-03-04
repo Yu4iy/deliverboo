47b5f8d3d66e7e7070c97a6dcde2edbe132d9cd7
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-03-04 01:07:03 +0100

    fix toggle menu in parte guest del sito

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -35,10 +35,7 @@
 @endsection
 
 @section('scripts')
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+   
 
     <!-- Nostro Js -->
     <script src="{{asset('js/front-office.js')}}"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -120,11 +120,11 @@
         </main>
     </div>
     @yield('vue')
+    @yield('scripts')
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    @yield('scripts')
     {{-- Script Js --}}
 
 </body>
