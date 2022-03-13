9b0852c6cf19241bef2722f66347eed8a5a7f326
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-03-13 20:06:27 +0100

    ok validazione con accenti per i dishes

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -34,7 +34,7 @@
                 <div class="mb-3">
                     <label for="ingredients" class="form-label">Ingredienti*</label>
                     <input class="form-control" type="text" id="ingredients" name="ingredients"
-                        value="{{old('ingredients', $dish->ingredients)}}" pattern="[a-zA-Z ,]+" required>
+                        value="{{old('ingredients', $dish->ingredients)}}" pattern="[a-zA-Zàèìòù ,]+" required>
                     <!-- Visualizzazione mirata per l'errore -->
                     @error('ingredients')
                     <div class="text-danger">{{$message}}</div>
